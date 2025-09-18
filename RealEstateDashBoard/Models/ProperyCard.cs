20dc68291c519a52aa232249fe3175ba35f08a6c
aKoranteng <akoranteng@msn.com>
2025-09-18 17:27:02 -0400

    Add project files.

---
+++ RealEstateDashBoard/Models/ProperyCard.cs
@@ -0,0 +1,11 @@
+﻿namespace RealEstateDashBoard.Models
+{
+    public  PropertyCard
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? ImageUrl { get; set; }
+        public decimal? Price { get; set; }
+    }
+}
